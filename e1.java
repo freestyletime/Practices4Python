6668424117a6b3f47db4d4b008f806f7e435def5
freestyletime <freestyletime@foxmail.com>
2021-10-17 15:29:15 +0100

    new solution

--- e1.java
+++ e1.java
@@ -7,12 +7,19 @@ public class e1 {
     public static void main(String[] args) {
         System.out.println(solution("abc", "bc"));
         System.out.println(solution("abc", "d"));
-        System.out.println(findIt(new int[]{0,1,0,1,0}));
-        System.out.println(Tickets(new int[]{25, 25, 50}));
-        System.out.println(Tickets(new int[]{25, 25, 50, 50, 100}));
+        System.out.println(findIt(new int[] { 0, 1, 0, 1, 0 }));
+        System.out.println(Tickets(new int[] { 25, 25, 50 }));
+        System.out.println(Tickets(new int[] { 25, 25, 50, 50, 100 }));
         System.out.println(order("4of Fo1r pe6ople g3ood th5e the2"));
         System.out.println(order2("Fo1r the2 g3ood 4of th5e pe6ople"));
         System.out.println(order("Empty input should return empty string"));
+        
+        System.out.println(GetSum(-1, 2));
+        System.out.println(GetSum(1, -4));
+        int[] result = twoSum(new int[]{1,2,3}, 4);
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+        System.out.println(knight("a3", "b5"));
     }
 
     public static boolean solution(String str, String ending) {
@@ -21,7 +28,7 @@ public class e1 {
 
     public static int findIt(int[] a) {
         int result = 0;
-        for(int n:a) {
+        for (int n : a) {
             result = n ^ result;
         }
         return result;
@@ -31,36 +38,40 @@ public class e1 {
         String yes = "YES";
         String no = "NO";
 
-        if (peopleInLine.length == 0) return no;
+        if (peopleInLine.length == 0)
+            return no;
         else {
             int ticket = 25;
             int c_25 = 0;
             int c_50 = 0;
             int c_100 = 0;
 
-            for (int m : peopleInLine){
+            for (int m : peopleInLine) {
                 int change = m - ticket;
                 switch (change) {
                     case 0:
                         c_25 += 1;
                         break;
                     case 25:
-                        if(c_25 > 0){
+                        if (c_25 > 0) {
                             c_25 -= 1;
                             c_50 += 1;
-                        } else return no;
+                        } else
+                            return no;
                         break;
                     case 75:
-                        if(c_25 > 0){
-                            if(c_50 > 0){
-                                c_25  -= 1;
-                                c_50  -= 1;
+                        if (c_25 > 0) {
+                            if (c_50 > 0) {
+                                c_25 -= 1;
+                                c_50 -= 1;
                                 c_100 += 1;
-                            }else if(c_25 > 2){
-                                c_25  -= 3;
+                            } else if (c_25 > 2) {
+                                c_25 -= 3;
                                 c_100 += 1;
-                            }else return no;
-                        } else return no;
+                            } else
+                                return no;
+                        } else
+                            return no;
                         break;
                     default:
                         return no;
@@ -73,34 +84,72 @@ public class e1 {
     public static String order(String words) {
         String result = "";
         int FLAG = 0;
-        if(words == null || words.isEmpty()) return result;
+        if (words == null || words.isEmpty())
+            return result;
         String[] arr = words.split(" ");
-        TreeMap<Character,String> map = new TreeMap<Character,String>();
+        TreeMap<Character, String> map = new TreeMap<Character, String>();
 
-        for(String word : arr){
+        for (String word : arr) {
             FLAG = 0;
             char[] chars = word.toCharArray();
-            for(char c : chars){
-                if(Character.isDigit(c)){
+            for (char c : chars) {
+                if (Character.isDigit(c)) {
                     FLAG = 1;
-                    if(map.containsKey(c)){
+                    if (map.containsKey(c)) {
                         map.put(c, map.get(c) + " " + word);
-                    }else{
+                    } else {
                         map.put(c, word);
                     }
                     break;
                 }
             }
-            if(FLAG == 0) result += word + " ";
+            if (FLAG == 0)
+                result += word + " ";
         }
 
-        for (Map.Entry<Character, String> entry : map.entrySet())  result += entry.getValue() + " ";
+        for (Map.Entry<Character, String> entry : map.entrySet())
+            result += entry.getValue() + " ";
         return result.substring(0, result.length());
     }
 
     public static String order2(String words) {
         return Arrays.stream(words.split(" "))
-          .sorted(Comparator.comparing(s -> Integer.valueOf(s.replaceAll("\\D", ""))))
-          .reduce((a, b) -> a + " " + b).get();
-      }
+                .sorted(Comparator.comparing(s -> Integer.valueOf(s.replaceAll("\\D", ""))))
+                .reduce((a, b) -> a + " " + b).get();
+    }
+
+    public static int GetSum(int a, int b) {
+        if (a > b) {
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+        }
+
+        int sum = 0;
+        while (a < b) sum += a++;
+        return sum + b;
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        Loop:for (int i = 0; i < numbers.length - 1; i++) {
+            for (int n = i + 1; n < numbers.length; n++) {
+                if((numbers[i] + numbers[n]) == target){
+                    return new int[] { i, n };
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * For information on knight moves, see https://en.wikipedia.org/wiki/Knight_%28chess%29
+     * For information on algebraic notation, see https://en.wikipedia.org/wiki/Algebraic_notation_%28chess%29
+     * @param p1 start position
+     * @param p2 end position
+     * @return the least step the knight should move
+     */
+    public static int knight(String p1, String p2){
+        
+        return 0;
+    }
 }
---
+++ e1.py
@@ -0,0 +1,34 @@
+# For information on knight moves, see https://en.wikipedia.org/wiki/Knight_%28chess%29
+# For information on algebraic notation, see https://en.wikipedia.org/wiki/Algebraic_notation_%28chess%29
+def knight(p1, p2):
+    direction = ((1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1))
+    boarder = [1,2,3,4,5,6,7,8]
+    
+    def caculation(x, y, p):
+        for m in direction:
+            tx = x + m[0]
+            ty = y + m[1]
+            if tx in boarder and ty in boarder and [tx, ty] not in p:
+                p.append([tx, ty])
+
+    sx, sy = ord(p1[0]) - 96, int(p1[1])
+    ex, ey = ord(p2[0]) - 96, int(p2[1])
+    # step / possibilities
+    step, p = 0, [[sx, sy]]
+
+    # caculation
+    if (ex - sx) == 0 and (ey - sy) == 0: return step
+    while True:
+        step += 1
+        s = len(p)
+        while s > 0:
+            s -= 1
+            f = p.pop(0)
+            caculation(f[0], f[1], p)
+        if [ex, ey] in p: return step
+
+##################################################################
+
+print(knight('a1', 'c1'))
+print(knight('a1', 'f4'))
+print(knight('a1', 'h8'))
