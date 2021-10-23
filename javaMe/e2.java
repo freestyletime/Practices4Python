8a83568e66c83a75731bfe010ec761cbc23b48c8
Chris Chen <mailchristianchen@gmail.com>
2021-10-23 23:46:59 +0100

    new practice

--- javaMe/e2.java
+++ javaMe/e2.java
@@ -12,6 +12,10 @@ public class e2 {
 
         System.out.println(spinWords("Welcome"));
         System.out.println(spinWords("Hey fellow warriors"));
+        System.out.println(factors(86240));
+        System.out.println(factors(7775460));
+        System.out.println(factors(10705716));
+
     }
 
     public static int[] parse(String data) {
@@ -33,5 +37,38 @@ public class e2 {
         return Arrays.stream(sentence.split(" "))
                 .map(n -> n.length() < 5 ? n : (new StringBuilder(n).reverse().toString()))
                 .reduce((a, b) -> a + " " + b).get();
+    }
+
+    /** Check the prime number */
+    static boolean isPrime(int n) {
+        if (n <= 1) return false; 
+        else if (n == 2) return true;
+        else if (n % 2 == 0) return false;
+        for (int i = 3; i <= Math.sqrt(n); i += 2) if (n % i == 0) return false;
+        return true;
+    }
+
+    public static String factors(int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 2;; i++) {
+            if(isPrime(n)) {
+                result.append("(" + n + ")");
+                return result.toString();
+            }
+            if (isPrime(i)) {
+                for (int r = 0, m = 1;; m++) {
+                    double p = Math.pow(i, m);
+                    if (n % p == 0)
+                        r = m;
+                    else if(r != 0){
+                        n /= Math.pow(i, r);
+                        if (r == 1) result.append("(" + i + ")");
+                        else result.append("(" + i + "**" + r + ")");
+                        if (n == 1) return result.toString();
+                        else break;
+                        } else break;
+                }
+            }
+        }
     }
 }
